78f0cf5b49266463548902211360f5ca484c36f4
Bolu Adetifa <boluadetifa@gmail.com>
2025-02-27 23:04:28 +0100

    fix(app): refactor application settings, server and example env files

--- backend/src/core/config/settings.ts
+++ backend/src/core/config/settings.ts
@@ -1,8 +1,6 @@
-import { ServerSettings } from "../../models/interfaces/settings.interfaces";
-
-const dotenv = require("dotenv");
-const path = require("path");
-const Joi = require("joi");
+import dotenv from "dotenv";
+import path from "path";
+import Joi from "joi";
 
 dotenv.config({ path: path.join(__dirname, "../../../.env") });
 
@@ -12,35 +10,33 @@ const envVarsSchema = Joi.object()
       .valid("PRODUCTION", "STAGING", "DEVELOPMENT", "TEST")
       .required(),
     SERVER_PORT: Joi.number().default(8000),
-    EMAIL_USERNAME: Joi.string().description("Email username "),
-    EMAIL_PASSWORD: Joi.string().description("Email password "),
-    EMAIL_FROM_ADDRESS: Joi.string().description("Zoho from address"),
-    AURORA_WEB_APP_BASE_URL: Joi.string().description(
-      "This is the base URL for the web app"
-    ),
+    JWT_SECRET_KEY: Joi.string().required().description("JWT Secret Key"),
+    BCRYPT_SALT_ROUNDS: Joi.number().required().description("Bcrypt Salt Rounds"),
+    EMAIL_USERNAME: Joi.string().required().description("Email username"),
+    EMAIL_PASSWORD: Joi.string().required().description("Email password"),
+    EMAIL_FROM_ADDRESS: Joi.string().required().description("Sender email address"),
+    AURORA_WEB_APP_BASE_URL: Joi.string().required().description("Base URL for Aurora Web App"),
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema
-  .prefs({ errors: { label: "key" } })
-  .validate(process.env);
+const { value: envVars, error } = envVarsSchema.validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
 }
 
-const serverSettings: ServerSettings = {
+const serverSettings = {
   serverEnvironment: envVars.SERVER_ENVIRONMENT,
   serverPort: envVars.SERVER_PORT,
-  bcryptHashingSalt: envVars.BCRYPT_HASHING_SALT,
   jwtSecretKey: envVars.JWT_SECRET_KEY,
+  bcryptHashingSalt: envVars.BCRYPT_SALT_ROUNDS,
+  auroraWebApp: {
+    baseUrl: envVars.AURORA_WEB_APP_BASE_URL,
+  },
   email: {
     username: envVars.EMAIL_USERNAME,
-    fromAddress: envVars.EMAIL_FROM_ADDRESS,
     password: envVars.EMAIL_PASSWORD,
-  },
-  auroraWebApp: {
-    baseUrl: envVars.AURORA_WEB_APP_BASE_URL
+    fromAddress: envVars.EMAIL_FROM_ADDRESS,
   },
 };
 
